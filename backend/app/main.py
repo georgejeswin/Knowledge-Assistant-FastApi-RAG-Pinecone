5ee155d4084eeed139f5e115fb1c4e762c58b0f7
Jeswin George <jeswin.george@airpay.technology>
2026-03-25 16:34:48 +0530

    Fixed cors issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.router import api_router
 from app.db.session import engine
 from app.db.base import Base
@@ -13,6 +14,14 @@ async def root():
 
 from app.db.base import Base
 from app.db.session import engine
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def on_startup():
